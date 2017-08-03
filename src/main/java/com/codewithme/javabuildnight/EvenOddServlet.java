2dc47bd79a05dd8d6f33261b9ba1653c0168e5bb
bisoldi <brooks.isoldi@traversed.com>
2017-08-02 22:46:08 -0400

    Added JSP page and servlet communicating with each other.

--- src/main/java/com/codewithme/javabuildnight/EvenOddServlet.java
+++ src/main/java/com/codewithme/javabuildnight/EvenOddServlet.java
@@ -1,5 +1,56 @@
 package com.codewithme.javabuildnight;
 
-public class EvenOddServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EvenOddServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD>");
+        out.println("<TITLE>Servlet Testing</TITLE>");
+        out.println("</HEAD>");
+        out.println("<BODY>");
+        out.println("Getting the Servlet Testing Center");
+        out.println("</BODY>");
+        out.println("</HTML>");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String parameter = request.getParameter("number");
+        try {
+            Long number = Long.valueOf(parameter);
+            request.setAttribute("number", number);
+            
+            if (isEven(number)) {
+                request.setAttribute("evenodd", "even");
+            } else {
+                request.setAttribute("evenodd", "odd");
+            }
+            
+            
+        } catch (Exception ex) {
+            request.setAttribute("evenodd", null);
+            request.setAttribute("msg", "Wait just a minute....You tried to trick me!!!");
+        }
+        
+        this.getServletContext().getRequestDispatcher("/evenodd.jsp").forward(request, response);
+    }
     
+    private boolean isEven(Long number) throws Exception {
+        if (Objects.isNull(number)) {
+            throw new Exception("Null value provided");
+        }
+        
+        return (number % 2) == 0;
+    }
+
 }
---
+++ src/main/webapp/evenodd.jsp
@@ -0,0 +1,30 @@
+<%@page import="java.util.Objects"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Even/Odd JSP Page</title>
+    </head>
+    <body>
+        <h1>Give me a number and I will rack my brain, crunch some numbers, ask the gods and tell you if it's even or odd</h1>
+        <form method="post" action="evenodd"> 
+            Please enter a number <br> 
+            <input type="text" name="number" size="5">
+            <input type="submit" value="submit"> 
+        </form> 
+    </body>
+    <%
+        Long number = (Long) request.getAttribute("number");
+        String evenOdd = (String) request.getAttribute("evenodd");
+        String msg = (String) request.getAttribute("msg");
+
+        if (Objects.nonNull(evenOdd)) {
+            out.println(number + " is... " + evenOdd);
+        }
+
+        if (Objects.nonNull(msg)) {
+            out.println(msg);
+        }
+    %>
+</html>
